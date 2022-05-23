2a3e601f3bbaa0a9532f06c6de2373cef0a3d044
Kavishar29 <1858920@students.wits.ac.za>
2022-05-23 14:23:32 +0200

    set up for socket and nodemon

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 
 const path = require('path')
 const express = require('express')
+const http = require('http')
 const app = express()
+const socketio = require('socket.io')
+const server = http.createServer(app)
+const io = socketio(server)
 const mainRouter = require('./routes/mainRoutes')
 app.use(mainRouter)
 
@@ -20,7 +24,3 @@ module.exports = app
 const port = process.env.PORT || 3000
 app.listen(port)
 console.log('Express server running on port', port)
-
-
-
-
