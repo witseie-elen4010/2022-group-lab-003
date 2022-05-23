07233769dbfd61c96cf441c5391739f949549a88
Zakhna Dhiraj <1831954@students.wits.ac.za>
2022-05-23 13:16:45 +0200

    Adding an extended word list #25

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,18 @@
 'use strict'
-
+import { wordList } from './Wordlist.js'
 function createWordleBoard () {
-  let board = document.getElementById('wordle-board')
+  const board = document.getElementById('wordle-board')
 
   // the board creation is the same as how a 2d array is created (nested-for loop).
   // first, the row is created and then each column in that row is made before moving to the next row.
   // the maximum number of tries that a player gets is 6 therefore the number of rows is 6.
   for (let i = 0; i < 6; i++) {
-    let row = document.createElement('div')
+    const row = document.createElement('div')
     row.className = 'row-part'
 
     // the length of each word can only be 5 hence why only 5 columns are created before the next row.
     for (let j = 0; j < 5; j++) {
-      let col = document.createElement('div')
+      const col = document.createElement('div')
       col.className = 'column-piece'
       row.appendChild(col)
     }
@@ -21,44 +21,11 @@ function createWordleBoard () {
   }
 }
 
-createWordleBoard();
+createWordleBoard()
 
-const wordList = [
-   "abuse",
-   "above",
-   "after",
-   "basic",
-   "chest",
-   "dance",
-   "earth",
-   "filed",
-   "grant",
-   "house",
-   "image",
-   "judge",
-   "knife",
-   "light",
-   "major",
-   "night",
-   "other",
-   "paper",
-   "phone",
-   "reply",
-   "scale",
-   "table",
-   "uncle",
-   "value",
-   "waste",
-   "youth",
-   "world",
-   "truth",
-   "range",
-   "chief",
-];
-
-const len = wordList.length;
-
-const chosenWord = wordList[Math.floor(Math.random() * len)];
+const len = wordList.length
+const chosenWord = wordList[Math.floor(Math.random() * len)]
+console.log(chosenWord)
 
 // keyboard CLICK input
 let tries = 6 // number of words that player is allowed to guess
@@ -154,39 +121,39 @@ function checkInput () {
     inputString += val
   }
 
-   if (inputString.length != 5) {
-      alert("Invalid: The word must be 5 letters long.");
-      return;
-   }
-  
-   if (
-      (!wordList.includes(inputString) &&
+  if (inputString.length != 5) {
+    alert('Invalid: The word must be 5 letters long.')
+    return
+  }
+
+  if (
+    (!wordList.includes(inputString) &&
          inputString !== chosenWord &&
          tries > 1) ||
       (wordList.includes(inputString) &&
          inputString !== chosenWord &&
          tries > 1)
-   ) {
-      alert("Invalid: the word is not on the list or is the incorrect word");
-      tries -= 1;
-      guess = [];
-      nextLetter = 0;
-      return;
-   }
+  ) {
+    alert('Invalid: the word is not on the list or is the incorrect word')
+    tries -= 1
+    guess = []
+    nextLetter = 0
+    return
+  }
 
-   if (inputString === chosenWord) {
-      alert("Correct! You win!");
-      tries = 0;
-   } else {
-      tries -= 1;
-      guess = [];
-      nextLetter = 0;
+  if (inputString === chosenWord) {
+    alert('Correct! You win!')
+    tries = 0
+  } else {
+    tries -= 1
+    guess = []
+    nextLetter = 0
 
-      if (tries === 0) {
-         alert("You lose. Guesses ran out.");
-         alert(`Correct word: "${chosenWord}"`);
-      }
-   }
+    if (tries === 0) {
+      alert('You lose. Guesses ran out.')
+      alert(`Correct word: "${chosenWord}"`)
+    }
+  }
 
   for (let i = 0; i < 5; i++) {
     let letterColour = ''
