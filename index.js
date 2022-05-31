2647d69b118bd2a577119cc0d1e5392035675167
grandmaJ <jessica.25.me@gmail.com>
2022-06-01 00:50:09 +0200

    Basic Login and Options pages

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', mainRouter)
-app.use('/public/stylesheets', express.static(__dirname + '/public/stylesheets'))
+app.use(
+   '/public/stylesheets',
+   express.static(__dirname + '/public/stylesheets')
+)
 app.use('/public/scripts', express.static(__dirname + '/public/scripts'))
 
 module.exports = app
@@ -20,7 +23,3 @@ module.exports = app
 const port = process.env.PORT || 3000
 app.listen(port)
 console.log('Express server running on port', port)
-
-
-
-
