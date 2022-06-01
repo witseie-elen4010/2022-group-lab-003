c3f4591e0261bc0eb139667824ef2319168439d8
Kavishar29 <1858920@students.wits.ac.za>
2022-06-01 15:06:34 +0200

    setting the db table

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -3,7 +3,7 @@
 const path = require('path')
 const express = require('express')
 const mainRouter = express.Router()
-const db = require('./database/db.js')
+
 
 mainRouter.get('/', function (req, res) {
    res.sendFile(path.join(__dirname, '..', 'views', 'login.html'))
@@ -24,28 +24,5 @@ mainRouter.get('/about', function (req, res) {
 mainRouter.get('/instructions', function (req, res) {
    res.sendFile(path.join(__dirname, '..', 'views', 'instructions.html'))
 })
-
-mainRouter.get('/database', function (req, res) {
-  // Make a query to the database
-  db.pools
-  // Run query
-  .then((pool) => {
-  return pool.request()
-  // This is only a test query, change it to whatever you need
-  .query('SELECT 1')
-  })
-  // Send back the result
-  .then(result => {
-  res.send(result)
-  })
-  // If there's an error, return that with some description
-  .catch(err => {
-  res.send({
-  Error: err
-  })
-  })
-  })
-  
-
 
 module.exports = mainRouter
