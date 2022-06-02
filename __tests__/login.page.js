e5fc303b392a601568d53e7eff692bc8102b284e
grandmaJ <jessica.25.me@gmail.com>
2022-06-03 01:09:40 +0200

    [AUTOMATED TEST] Clicking log in button with incorrect password (length 3) does not allow user to go to Options page

--- __tests__/login.page.js
+++ __tests__/login.page.js
@@ -17,4 +17,21 @@ describe('Login page click tests', () => {
 
       driver.close()
    })
+
+   test('Clicking the Log in button does not allow user to go to the Options page with incorrect password (only 3 letters)', async function () {
+      let driver = await new Builder().forBrowser('chrome').build()
+      await driver.get('https://multi-wordle.azurewebsites.net/')
+
+      driver.findElement(By.id('username')).sendKeys('testing')
+      driver.findElement(By.id('password')).sendKeys('czk')
+
+      await driver.findElement(By.xpath('(//*[@id="linkButton2"])[1]')).click()
+
+      let expectedTitle = 'Multi-Wordle Login Page'
+      let actualTitle = await driver.getTitle()
+
+      assert.equal(actualTitle, expectedTitle)
+
+      driver.close()
+   })
 })
