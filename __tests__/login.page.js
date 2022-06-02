9f8979e5f56ec528bd41a4eae94b295825d1b708
grandmaJ <jessica.25.me@gmail.com>
2022-06-03 01:12:50 +0200

    [AUTOMATED TEST] Clicking log in button with incorrect password (length 8 but only lowercase and uppercase) does not allow user to go to Options page

--- __tests__/login.page.js
+++ __tests__/login.page.js
@@ -34,4 +34,21 @@ describe('Login page click tests', () => {
 
       driver.close()
    })
+
+   test('Clicking the Log in button does not allow user to go to the Options page with incorrect password (only lowercase and uppercase)', async function () {
+      let driver = await new Builder().forBrowser('chrome').build()
+      await driver.get('https://multi-wordle.azurewebsites.net/')
+
+      driver.findElement(By.id('username')).sendKeys('testing')
+      driver.findElement(By.id('password')).sendKeys('czkABDCD')
+
+      await driver.findElement(By.xpath('(//*[@id="linkButton2"])[1]')).click()
+
+      let expectedTitle = 'Multi-Wordle Login Page'
+      let actualTitle = await driver.getTitle()
+
+      assert.equal(actualTitle, expectedTitle)
+
+      driver.close()
+   })
 })
