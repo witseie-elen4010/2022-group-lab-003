32248fb23a5e0433b74e9b28fa5904210524fd3f
grandmaJ <jessica.25.me@gmail.com>
2022-06-03 01:21:26 +0200

    [AUTOMATED TEST] Clicking log in button with incorrect username (length <3) does not allow user to go to Options page

--- __tests__/login.page.js
+++ __tests__/login.page.js
@@ -68,4 +68,21 @@ describe('Login page click tests', () => {
 
       driver.close()
    })
+
+   test('Clicking the Log in button does not allow user to go to the Options page with incorrect username (length<3)', async function () {
+      let driver = await new Builder().forBrowser('chrome').build()
+      await driver.get('https://multi-wordle.azurewebsites.net/')
+
+      driver.findElement(By.id('username')).sendKeys('te')
+      driver.findElement(By.id('password')).sendKeys('czkABDcsa1')
+
+      await driver.findElement(By.xpath('(//*[@id="linkButton2"])[1]')).click()
+
+      let expectedTitle = 'Multi-Wordle Login Page'
+      let actualTitle = await driver.getTitle()
+
+      assert.equal(actualTitle, expectedTitle)
+
+      driver.close()
+   })
 })
