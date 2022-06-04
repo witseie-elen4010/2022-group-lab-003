97537004d48d89a4e645d51f297000a119af62a6
grandmaJ <jessica.25.me@gmail.com>
2022-06-04 14:31:49 +0200

    Testing playwright with our game website

---
+++ tests/instructions.test.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const { firefox } = require('playwright')
+let browser
+let page
+
+// Needs to be higher than the default Playwright timeout
+jest.setTimeout(40 * 1000)
+
+beforeAll(async () => {
+   browser = await firefox.launch()
+})
+afterAll(async () => {
+   await browser.close()
+})
+beforeEach(async () => {
+   page = await browser.newPage()
+})
+afterEach(async () => {
+   await page.close()
+})
+
+describe('Instruction page clicking tests', () => {
+   test('Instruction page opens therefore title is MULTI-WORDLE', async () => {
+      await page.goto('https://multi-wordle.azurewebsites.net/instructions')
+      expect(await page.title()).toBe('MULTI-WORDLE')
+   })
+})
