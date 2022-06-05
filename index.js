d64aec32e19540382d13d4cb3404b204c948fbbd
Kavishar29 <1858920@students.wits.ac.za>
2022-06-05 10:57:06 +0200

    login trail to get input

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const mainRouter = require('./routes/mainRoutes')
+const users = []
+const bcrypt = require('bcrypt')
+app.use(express.urlencoded({ extened: false})) // tells us we want to access our inputs
+
 
 app.use(mainRouter)
 
@@ -13,13 +17,28 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/', mainRouter)
-app.use(
-   '/public/stylesheets',
-   express.static(__dirname + '/public/stylesheets')
-)
+app.use('/public/stylesheets',express.static(__dirname + '/public/stylesheets'))
 app.use('/public/scripts', express.static(__dirname + '/public/scripts'))
 app.use('/database', express.static(__dirname + '/database'))
 app.use('/routes', express.static(__dirname + '/routes'))
+
+app.post('/login',async (req,res) => {
+   try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10) // standard default value
+      users.push({
+         username: req.bady.username,
+         password: hashedPassword
+      })
+   } catch {
+      res.redict('/login')
+
+   }
+   console.log(users)
+})
+
+
+
+
 module.exports = app
 
 const port = process.env.PORT || 3000
