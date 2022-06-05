79a02e5f9642d309640f6bf6b4bbb82eb44a2094
Kavishar29 <1858920@students.wits.ac.za>
2022-06-05 16:06:13 +0200

    store username input into the database

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const db = require('./database/db.js')
 
 app.use(mainRouter)
 
-const bodyParser = require('body-parser')
+var bodyParser = require('body-parser')
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', mainRouter)
 app.use('/public/stylesheets',express.static(__dirname + '/public/stylesheets'))
@@ -21,36 +21,35 @@ app.use('/public/scripts', express.static(__dirname + '/public/scripts'))
 app.use('/database', express.static(__dirname + '/database'))
 app.use('/routes', express.static(__dirname + '/routes'))
 
-app.post('/',async (req,res) => {
-   try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10) // standard default value
-      users.push({
-         username: req.body.username,
-         password: hashedPassword //hased password to store in our database because it is safer
-      })
-      res.redirect('/options')
-   } catch {
-      res.redirect('/') //incase of a failure redirect back to main page i.e. login
+// app.post('/',async (req,res) => {
+//    try {
+//       const hashedPassword = await bcrypt.hash(req.body.password, 10) // standard default value
+//       users.push({
+//          username: req.body.username,
+//          password: hashedPassword //hased password to store in our database because it is safer
+//       })
+//       res.redirect('/options')
+//    } catch {
+//       res.redirect('/') //incase of a failure redirect back to main page i.e. login
 
-   }
-   console.log(users) //check when added a user into the game
-})
+//    }
+//    console.log(users) //check when added a user into the game
+// })
 
-app.get('/database', function (req, res) { //when you go into /database it updates the database
+app.post('/', function (req, res) { //when you go into /database it updates the database
    
-   let user = users.username
-   let pass = users.password
+   let user = req.body.username
+   let pass = req.body.password
+   const hashedPassword = bcrypt.hash(req.body.password, 10) // standard default value
    // Make a query to the database
     db.pools
     // Run query
     .then((pool) => {
     return pool.request()
-    .query(`INSERT INTO Login(username, password) VALUES('${user}','${pass}');`)
+    .query(`INSERT INTO Login(username, password) VALUES('${user}','${hashedPassword}');`)
     })
     // Send back the result
-    .then(result => {
-    res.send(result)
-    })
+    .then(res.redirect('/options'))
     // If there's an error, return that with some description
     .catch(err => {
     res.send({
