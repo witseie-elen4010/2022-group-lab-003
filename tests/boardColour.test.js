43d56faafc07bd9817de71900b9c0ed564b3418b
faatimahomed <faatimahomed0897@gmail.com>
2022-06-05 20:52:01 +0200

    test game board colour changes for row 4 and 5

--- tests/boardColour.test.js
+++ tests/boardColour.test.js
@@ -81,7 +81,48 @@ describe('Game board changes colour after clicking "Enter"', () => {
          expect(color2).toHaveText['rgb(0, 0, 255)','rgb(128, 128, 128)','rgb(255, 192, 203)']
         })
 
+        test('Test third box, fourth  row changes colour', async () => {
+            await page.goto('https://multi-wordle.azurewebsites.net/game')
+            for(let i=0; i<4; i++){
+            await page.locator('button:has-text("m")').click()
+            await page.locator('button:has-text("u")').click()
+            await page.locator('button:has-text("s")').click()
+            await page.locator('button:has-text("i")').click()
+            await page.locator('button:has-text("c")').click()
+            await page.locator('button:has-text("Enter")').click()
+            }
+     
+            const box3 = await page.locator('(//*[@class="column-piece"])[18]')
+            const color3 = await box3.evaluate((e) => {
+            return window.getComputedStyle(e).getPropertyValue('background-color')
+         })
+     
+         expect(color3).toHaveText['rgb(0, 0, 255)','rgb(128, 128, 128)','rgb(255, 192, 203)']
+        })
         
+        test('Test third box, fifth row does not change colour', async () => {
+            await page.goto('https://multi-wordle.azurewebsites.net/game')       
+            for(let i=0; i<5; i++){
+            await page.locator('button:has-text("m")').click()
+            await page.locator('button:has-text("u")').click()
+            await page.locator('button:has-text("s")').click()
+            await page.locator('button:has-text("i")').click()
+            await page.locator('button:has-text("c")').click()
+            await page.locator('button:has-text("Enter")').click()
+            }
+            await page.locator('button:has-text("m")').click()
+            await page.locator('button:has-text("u")').click()
+            await page.locator('button:has-text("s")').click()
+            await page.locator('button:has-text("i")').click()
+            await page.locator('button:has-text("Enter")').click()
+     
+            const box5 = await page.locator('(//*[@class="column-piece"])[18]')
+            const color5 = await box5.evaluate((e) => {
+            return window.getComputedStyle(e).getPropertyValue('background-color')
+         })
+     
+         expect(color5).toHaveText['rgb(156, 155, 155']
+        })
          
 })
 
